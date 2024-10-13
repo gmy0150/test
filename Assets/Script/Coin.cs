3959bc19de594d2a520f51437445cf54c0c4c75d
gmy0150 <gmy0150@naver.com>
2024-10-14 01:38:36 +0900

    코인
    코인

---
+++ Assets/Script/Coin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
